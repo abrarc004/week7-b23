dad0651c6afb364efa8080f373ef3b28b96550d3
abrarc004 <82032419+abrarc004@users.noreply.github.com>
2021-05-26 18:45:54 +0800

    Create hotels-edit.vue

---
+++ hotels-edit.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+   <h1>Edit page</h1>
+   <div class="container">
+   	<div class="card bg-light p-3">
+   	
+   	</div>
+   </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Edit',
+ 
+}
+</script>
