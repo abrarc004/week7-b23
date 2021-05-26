32c36a251f334bb2cfceec4717a40e2b3930d7d8
abrarc004 <82032419+abrarc004@users.noreply.github.com>
2021-05-26 18:47:05 +0800

    Create hotels-detail.vue

---
+++ hotels-detail.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+   <h1>{{hotel.name}}</h1>
+   <p>Price: {{hotel.price}}</p>
+   <p>Location : {{hotel.location}}</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Detail',
+  mounted(){
+  	let url = 'https://api.sheety.co/14e54678109837e42cb17c0e97377090/hotels/sheet1';
+fetch(url)
+.then((response) => response.json())
+.then(json => {
+  // Do something with the data
+  this.hotels =json.sheet1;
+});
+  }
+  data (){
+  return{
+name:'',
+ price:'',
+ location:'',
+ type:'',
+ imgurl:'',
+  }
+  },
+ 
+}
+</script>
